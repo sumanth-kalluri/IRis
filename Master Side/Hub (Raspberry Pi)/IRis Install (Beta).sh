273fc6334c9d44171e817734b4a2dcf46ac5ccb4
Nishad Mandlik <mandliksg@gmail.com>
2018-06-13 22:37:31 +0530

    Installation Script Dependencies Done

--- Master Side/Hub (Raspberry Pi)/IRis Install (Beta).sh
+++ Master Side/Hub (Raspberry Pi)/IRis Install (Beta).sh
@@ -13,9 +13,19 @@ sudo apt-get install python-qt4
 
 #Make Directory In /var/www/html/
 sudo mkdir /var/www/html/Login_v2/
-sudo chmod 777 -R /var/www/html/Login_v2
 
 #Copy All Files To Apache2 Directory
 cp -R ./Login_v2/* /var/www/html/Login_v2/
+
+#Own The Site Directory
+sudo chown -R $(whoami) /var/www/html/Login_v2
+
+#Change Permissions to 777
+sudo chmod 777 -R /var/www/html/Login_v2
+
+#Ask User For Password Of MySQL & PHPMyAdmin
+read -sp 'Please Enter The Password Used For MySQL: ' userPass
+
+#Change The Password In The
 
 #Edit .bashrc and rc.local files
--- Master Side/Hub (Raspberry Pi)/Login_v2/database/db_connect.php
+++ Master Side/Hub (Raspberry Pi)/Login_v2/database/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 define('db_host', 'localhost');
 define('db_user', 'root');
-define('db_password', '');
+define('db_password', '9833274147');
 define('db_name', 'whiz');
 $con = mysqli_connect(db_host, db_user, db_password, db_name) or die("Couldn't connect.");
 ?>
